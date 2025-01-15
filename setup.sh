f19c9643de6d3c570d3b49f00c565364593c88c2
harrissusanto91 <119999193+harrissusanto91@users.noreply.github.com>
2025-01-15 21:06:45 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash  
-
-# Customize the Bash prompt to show one directory after 'workspaces'  
-PS1='$(if [[ "$PWD" == /workspaces* ]]; then echo "${PWD#/workspaces} $ "; else echo "$PWD $ "; fi)'  
-echo "export PS1='$PS1'" >> ~/.bashrc  
